6c24a80adfaccd298fa3a18cc2f4ac6656c50d5b
Mangachh <mangachh@gmail.com>
2023-06-29 16:38:51 +0200

    datauser implements userDetails

--- src/main/java/cbs/cine_foro/entity/security/DataUser.java
+++ src/main/java/cbs/cine_foro/entity/security/DataUser.java
@@ -1,8 +1,17 @@
 package cbs.cine_foro.entity.security;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.annotation.Generated;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -21,7 +30,7 @@ import lombok.NoArgsConstructor;
 @Table(name = "data_users",
         uniqueConstraints = { @UniqueConstraint(name = "unique_data_user_name", columnNames = "user_name"),
                               @UniqueConstraint(name = "unique_data_user_email", columnNames = "email")})
-public class DataUser {
+public class DataUser implements UserDetails {
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,4 +40,43 @@ public class DataUser {
     private String userName;
     private String email;
     private String password;
+
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+    
+    @Override
+    public String getUsername() {
+        return this.userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        // negation!
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override // from interface
+    public String getPassword() {
+        return this.password;
+    }
 }
---
+++ src/main/java/cbs/cine_foro/entity/security/Role.java
@@ -0,0 +1,6 @@
+package cbs.cine_foro.entity.security;
+
+public enum Role {
+    USER,
+    ADMIN
+}
